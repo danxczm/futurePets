21d6d5bb78be469c06b3cd2f51b2d0a8b0c8c72c
dan <81313913+danxczm@users.noreply.github.com>
2022-12-11 08:52:44 +0200

    color control

--- colors/js/app.js
+++ colors/js/app.js
@@ -5,19 +5,56 @@ const refs = {
   currentHexes: document.querySelectorAll(".color h2"),
 };
 let initialColors;
-const generateHex = _ => chroma.random();
+const generateHex = (_) => chroma.random();
 
-function randomColor() {
-	refs.colorDivs.forEach((div, index) => {
-		const hexText = div.children[0];
-		// const randomColor = generateHex();
-		div.style.backgroundColor = generateHex();
-		hexText.innerText = generateHex();
-	})
+function randomColors() {
+  refs.colorDivs.forEach((div, index) => {
+    const hexText = div.children[0];
+    //add the color
+    div.style.backgroundColor = generateHex();
+    hexText.innerText = generateHex();
+    //change the text color
+    checkTextColor(generateHex(), hexText);
+    //colorize sliders
+    const color = chroma(generateHex());
+    const sliders = div.querySelectorAll(".sliders input");
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const saturation = sliders[2];
+
+    colorizeSliders(color, hue, brightness, saturation);
+  });
 }
 
-randomColor()
+function checkTextColor(color, text) {
+  const brightness = chroma(color).luminance();
+  brightness > 0.5
+    ? (text.style.color = "black")
+    : (text.style.color = "white");
+}
 
+function colorizeSliders(color, hue, brightness, saturation) {
+  //saturation
+  const lowSat = color.set("hsl.s", 0);
+  const maxSat = color.set("hsl.s", 1);
+  const scaleSat = chroma.scale([lowSat, color, maxSat]);
+  //srightness
+  const midBright = color.set("hsl.l", 0.5);
+  const scaleBright = chroma.scale(["black", midBright, "white"]);
+
+  //update colors
+  saturation.style.backgroundImage = `linear-gradient(to right,${scaleSat(
+    0
+  )}, ${scaleSat(1)})`;
+  brightness.style.backgroundImage = `linear-gradient(to right,${scaleBright(
+    0
+  )},${scaleBright(0.5)} ,${scaleBright(1)})`;
+
+  //hue
+  hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75),rgb(204,204,75),rgb(75,204,75),rgb(75,204,204),rgb(75,75,204),rgb(204,75,204),rgb(204,75,75))`;
+}
+
+randomColors();
 /* generate random color:
 	const letters = "0123456789ABCDEF";
   let hash = "#";
@@ -26,4 +63,3 @@ randomColor()
   hash += letters[Math.floor(Math.random() * 16)];
   }
 	*/
-
